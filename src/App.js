fa648214f11f0f9bb08353e9854d6a49f7fe7274
sam-bandyopadhyay <rwijub@gmail.com>
2021-12-04 08:25:54 -0700

    All filtering stratagems by Phase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactDOM from 'react-dom';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import stratagems from './stratagems/deathGuard.json';
+import { buttons } from "./buttons.js";
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+function MakeCardGrid(stratagems) {
+  return (
+    <Grid container spacing={4}>
+      {stratagems.map(stratagem => (
+        <Grid item xs={12} sm={6} md={4}>
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {stratagem.name}
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                {stratagem.cost}
+              </Typography>
+              <Typography variant="body2">
+                {stratagem.description}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
+
+function filterStratagems(stratagemType) {
+  let filteredStratagems = stratagems.filter(stratagem => stratagem.phases.includes(stratagemType));
+  return filteredStratagems;
+}
 
 function App() {
+  const [filteredStratagems, setFilteredStratagems] = useState([]);
+  useEffect(() => {
+    setFilteredStratagems(stratagems);
+  }, []);
+
+  function handleStratagems(e) {
+    let stratagemType = e;
+    stratagemType !== "All"
+      ? setFilteredStratagems(filterStratagems(stratagemType))
+      : setFilteredStratagems(stratagems);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        {buttons &&
+          buttons.map((type) => (
+            <Button variant="outlined" onClick={() => handleStratagems(type.value)}>
+              {type.name}
+            </Button>
+          ))}
+      </Stack>
+      {MakeCardGrid(Object.values(filteredStratagems))}
+    </>
+  )
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
---
+++ src/buttons.js
@@ -0,0 +1,14 @@
+export const buttons = [
+    {
+        name: "All",
+        value: "All"
+    },
+    {
+        name: "Shooting",
+        value: "Shooting"
+    },
+    {
+        name: "Fight",
+        value: "Fight"
+    }
+];
